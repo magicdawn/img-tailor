d5033f17b553ce0a4c484b442dfab5d7456b282a
magicdawn <magicdawn@qq.com>
2021-08-22 00:20:05 +0800

    chore: clean up

--- src/bin.ts
+++ src/bin.ts
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
 
-import canvas from 'canvas'
-import clipanion from 'clipanion'
-import sharp from 'sharp'
-import exifr from 'exifr'
-import moment from 'moment'
-import fs from 'fs-extra'
-import mozjpeg from 'node-mozjpeg'
-import MeasureTimer from './Timer'
 import debugFactory from 'debug'
 import {Cli, Builtins} from 'clipanion'
 import {DateCommand} from './commands/date.js'
